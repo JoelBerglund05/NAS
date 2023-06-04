81ca64cc50f9bbbdb7490f207aee8c2d294c9d76
Joel Berglund <joelberglund4497@gmail.com>
2023-06-04 11:50:42 +0200

    sfwerf

--- website/humidity.py
+++ website/humidity.py
@@ -1,24 +1,37 @@
 import datetime
-from flask import Blueprint, request
-from sense_hat import SenseHat
+from flask import Blueprint, render_template, request
+from flask_login import current_user
 from . import db
 from .models import EnviromentDetails
 
-sense = SenseHat()
-sense.clear()
 
 humidityBp = Blueprint('humidity', __name__)
-humidity = sense.get_humidity()
-print(humidity)
 
-@humidityBp.route('/Upload', methods = ['GET', 'POST'])
+@humidityBp.route('/Upload', methods = ['POST'])
 def UploadData():
-    data = request.json
+    humidity_data = request.form['humidity']
+    time = datetime.date.today()
+    location = request.form['location']
+    data = {
+        'data': {
+            'humidity':humidity_data,
+            'location':location,
+            'time':time,
+        }
+    }
+    EnviromentDetails(humidity = data[0], location = data[1], dateTime = data[2])
+    return data
 
-    mockhumidity = 42.5
-    mockdateTime = datetime.date.today()
-    mocklocation = 'trät'
-    
-    request = EnviromentDetails(humidity = mockhumidity, dateTime = mockdateTime, location = mocklocation)
-    db.session.add(request)
-    db.session.commit()
+
+@humidityBp.route('/viewdata', methods = ['GET',])
+def home():
+    return render_template('view_data.html')
+
+
+#mockhumidity = 42.5
+#mockdateTime = datetime.date.today()
+#mocklocation = 'trät'
+#
+#request = EnviromentDetails(humidity = mockhumidity, dateTime = mockdateTime, location = mocklocation)
+#db.session.add(request)
+#db.session.commit()
