7d95bfe296a44380073f0f246ad9a2a457ba770b
Joel Berglund <joelberglund4497@gmail.com>
2023-06-04 17:10:53 +0100

    seensor

--- sensor_code/sensor.py
+++ sensor_code/sensor.py
@@ -1,12 +1,9 @@
 
 from sense_hat import SenseHat
-import time
 
 def value():
     sense = SenseHat()
     sense.clear()
     humidity = sense.get_humidity()
     return humidity
-    time.sleep(30)
-
 
--- sensor_code/transfer.py
+++ sensor_code/transfer.py
@@ -1,20 +1,20 @@
 import requests
 from sense_hat import SenseHat
 from sensor import value
-
-while value:
-    return humidity
+import time
 
 
-
-data = {
+while True:
+    data = {
     'humidity' : value(),
     'location' : 'swe'
-}
+    }
 
-formResponse = requests.post(
-    url = 'http://192.168.0.49:5000/Upload',
-    data=data ,
-    headers= {
-        'Content-Type': 'application/x-www-form-urlencoded'#
-    })
+    formResponse = requests.post(
+        url = 'http://192.168.0.49:5000/Upload',
+        data=data ,
+        headers= {
+            'Content-Type': 'application/x-www-form-urlencoded'#
+        })
+    time.sleep(300)
+    
