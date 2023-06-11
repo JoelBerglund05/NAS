63c54f3997caaaa5ab0bae8c4ac9cb07e6983d3b
Joel Berglund <joelberglund4497@gmail.com>
2023-06-11 20:27:39 +0200

    yuiu

--- sensor_code/sensor.py
+++ sensor_code/sensor.py
@@ -4,7 +4,7 @@ from sense_hat import SenseHat
 def value():
     sense = SenseHat()
     sense.clear()
-    humidity = sense.get_humidity()
+    humidity = sense.humidity
     return humidity
 
 sense = SenseHat()
